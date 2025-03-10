a991aafe80feffaaad12a10154b43bcb409322f0
MichaelPells <themichaelpells@gmail.com>
2025-03-10 18:04:12 +0100

    Wrote main algorithm

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import sys
 
 def main():
-    ...
+    integers = [2, -2, 5, -1]
+
+    integers = filter(lambda Yn: Yn >= 0, integers)
+    integers = map(lambda Yn: Yn**2, integers)
+    result = sum(integers)
+
+    print(result)
 
 
 if __name__ == "__main__":
