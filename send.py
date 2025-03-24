90266830e86a2c3fa5fc47fde2c1f973e077dc4f
MichaelPells <themichaelpells@gmail.com>
2025-03-24 17:06:59 +0100

    Did Mission 2

--- send.py
+++ send.py
@@ -1,11 +1,13 @@
 import requests
 
-url = "https://api.challenge.hennge.com/challenges/003"
+URL = "https://api.challenge.hennge.com/challenges/003"
 
 body = {
-  "github_url": "https://gist.github.com/MichaelPells/GIST_ID",
+  "github_url": "https://gist.github.com/MichaelPells/dc6a80a309a866999c259645cfec65ce",
   "contact_email": "themichaelpells@gmail.com",
   "solution_language": "python"
 }
 
-# requests.post()
+requests.post(URL,
+              json = body
+              )
