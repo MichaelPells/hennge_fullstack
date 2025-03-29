59f2ca7350affb936b4c37433bfb67a3e90fa598
MichaelPells <themichaelpells@gmail.com>
2025-03-29 01:03:10 +0100

    Added comments

--- main.py
+++ main.py
@@ -8,15 +8,15 @@ M = Total number of integers in all test cases
 """
 
 
-def adder(cases, N, test, output, depth, maxdepth):
+def adder(cases: list, N: int, test: int, output: list, depth: int, maxdepth: int):
     """
-    `adder` function:
-    - Step 1: Extracts test case.
-    - Step 2: Filters out negative integers (`Yn`).
-    - Step 3: Squares all integers.
-    - Step 4: Sums up all integers `(Y1 + Y2 + ... + Yn)`.
-    - Step 5: Stores result to `output`.
-    - Step 6: Calls another `adder` function on next test case, if any, or end recursion, if none.
+    Finds the next test case, adds up the squares of the non-negative integers, and stores result in `output`.
+    - `cases`:
+    - `N`:
+    - `test`:
+    - `output`:
+    - `depth`:
+    - `maxdepth`:
     """
 
     # Step 1: Extracts test case.
@@ -35,44 +35,53 @@ def adder(cases, N, test, output, depth, maxdepth):
     # Step 5: Stores result to `output`.
     output.append(str(result)) # Append result to `output` object (as a string for `str.join()`).
 
-    # Step 6: Calls another `adder` function on next test case, if any, or end recursion, if none.
-    if test < N:
-        if depth < maxdepth: adder(cases, N, test + 1, output, depth + 1, maxdepth)
-
-def recursion_depth_manager(cases, N, test, output, depth, maxdepth):
-    if test < N:
+    # Step 6: Calls another `adder` function on next test case if any,
+    # or end recursion if none, or when maxdepth is reached.
+    # This terminates current recursion if our maximum recursion depth (maxdepth) is reached.
+    if test < N and depth < maxdepth:
         adder(cases, N, test + 1, output, depth + 1, maxdepth)
 
-        if test + maxdepth < N:
-            recursion_depth_manager(cases, N, test + maxdepth, output, 0, maxdepth)
+def recursion_manager(cases: list, N: int, test: int, output: list, depth : int = 0, maxdepth : int = 5):
+    """
+    Manages `adder`'s call stack, ensuring recursion depth slowly approaches `sys.getrecursionlimit()`.
+    Breaks recursion into a series of smaller recursions in sizes of `maxdepth`,
+    greatly inhibiting stack overflow and reducing space complexity to `O(N / maxdepth)`.
+    """
+
+    # Do `test + 1` for first (or next) test case,
+    # and call `adder` function with all needed args.
+    # This creates a recursion of `maxdepth` number of function calls
+    # that treats only the next `maxdepth` number of test cases,
+    # and stores each test result in `output`.
+    # `output` object is provided by reference for a more efficient update to `output` across recursions.
+    adder(cases, N, test + 1, output, depth + 1, maxdepth)
+
+    # Start another recursion of `maxdepth` number of function calls
+    # if there are still test cases left.
+    if test + maxdepth < N:
+        recursion_manager(cases, N, test + maxdepth, output) # Do `test + maxdepth` for next test case
 
 def main():
     """
-    `main` function:
-    - Step 1: Reads the standard input for test cases.
-    - Step 2: Calls `adder` function on first test case.
-    - Step 3: Outputs program result to standard output.
+    Program entry point. Reads data from standard input, passes data to `recursion_manager`,
+    and prints final result to standard output.
     """
 
     # Step 1: Reads the standard input for test cases.
     data = open(0) # File descriptor `0` points to standard input
-    N = int(data.readline()) # Reads `N` test cases on first line
+    N = int(data.readline().strip()) # Reads `N` test cases on first line
     cases = data.read().splitlines() # Reads the rest of the input (test cases) as a list of lines
 
     # Step 2: Calls `adder` function on first test case.
     test = 0 # Initialize number of test cases treated with `0`.
     output = [] # Create an empty list to store result of each test case.
 
-    # Do `test + 1` for first test case,
-    # and call `adder` function with all needed args.
-    # This creates a recursion that treats all test cases
-    # and stores all test results in `output`.
-    # `output` object is intentionally provided by reference, for a more efficient update to `output` across recursion.
-    recursion_depth_manager(cases, N, test, output, 0, 5)
+    # Start a recursion of `maxdepth` number of function calls if there are test cases.
+    if N: recursion_manager(cases, N, test, output)
 
     # Step 3: Outputs program result to standard output.
     # Join all results in `output` with `\n`.
-    open(1, "w").write("\n".join(output)) # File descriptor `1` points to standard output.
+    open(1, "w").write("\n".join(output) + "\n") # File descriptor `1` points to standard output.
 
 if __name__ == "__main__":
     main()
