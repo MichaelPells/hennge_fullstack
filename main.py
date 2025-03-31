e31c8254d061a158d1fd31bc56dc5a2ac74dbc3b
MichaelPells <themichaelpells@gmail.com>
2025-03-31 16:41:22 +0100

    Renamed variables and rewrote comments

--- main.py
+++ main.py
@@ -11,15 +11,15 @@ I devised a technique I call Recursion Breaking. Works like magic!
 """
 
 
-def adder(cases: list, N: int, test: int, output: list, maxdepth: int, depth: int):
+def adder(cases: list, N: int, test: int, output: list, maxframes: int, frames: int):
     """
     Finds the next test case, adds up the squares of the non-negative integers, and stores result in `output`.
     - `cases`: List of all test cases
     - `N`: Number of test cases
     - `test`: Index of current test case (starting at `1`)
     - `output`: List containing result from each test case
-    - `maxdepth`: Maximum recursion depth allowed per recursion
-    - `depth`: Recursion depth for current function call in each recursion's call stack
+    - `maxframes`: Maximum number of stack frames allowed per recursion
+    - `frames`: Current number of stack frames in current recursion
     """
 
     # Step 1 - Extract test case:
@@ -39,42 +39,43 @@ def adder(cases: list, N: int, test: int, output: list, maxdepth: int, depth: in
     output.append(f"{result}\n") # Append result to `output` object (as a string for `str.join()`).
 
     # Step 6 - Call another `adder` function on next test case if any,
-    # or end recursion if none, or when maxdepth is reached.
-    # This temporarily increases the recursion depth by `maxdepth - depth` more number of `adder` function calls,
-    # and terminates current recursion if the set maximum recursion depth (`maxdepth`) is reached:
-    if test < N and depth < maxdepth:
-        adder(cases, N, test + 1, output, maxdepth, depth + 1)
+    # or end recursion if none, or when maxframes is reached.
+    # This temporarily increases the recursion depth by `maxframes - frame` more number of `adder` function calls,
+    # and terminates current recursion if the set maximum number of stack frames (`maxframes`) is reached:
+    if test < N and frames < maxframes:
+        adder(cases, N, test + 1, output, maxframes, frames + 1)
 
-def recursion_manager(cases: list, N: int, test: int, output: list, maxdepth : int):
+def recursion_manager(cases: list, N: int, test: int, output: list, maxframes : int):
     """
     Manages `adder`'s call stack, ensuring recursion depth slowly approaches `sys.getrecursionlimit()`.
-    Breaks recursion into a series of smaller recursions in sizes of `maxdepth`,
-    greatly inhibiting stack overflow and reducing a space complexity of `O(N)` to something like `O(N / maxdepth)`.
+    Breaks recursion into a series of smaller recursions in sizes of `maxframes`,
+    greatly inhibiting stack overflow and empirically reducing a space complexity of `O(N)`
+    to something like `O(N / maxframes)`.
     That is, the recursion depth only actually increases by `1` `recursion_manager` function call
-    for every `maxdepth` number of `adder` function calls.
+    for every `maxframes` number of `adder` function calls.
     - `cases`: List of all test cases
     - `N`: Number of test cases
     - `test`: Index of last test case from last recursion.
     - `output`: List containing result from each test case
-    - `maxdepth`: Maximum recursion depth allowed per recursion
+    - `maxframes`: Maximum number of stack frames allowed per recursion
     """
 
     # Do `test + 1` for first (or next) test case,
     # and call `adder` function with all needed args.
-    # This creates a recursion of `maxdepth` number of `adder` function calls
-    # that treats only the next `maxdepth` number of test cases,
+    # This creates a recursion of `maxframes` number of `adder` function calls
+    # that treats only the next `maxframes` number of test cases,
     # and stores each test case result in `output`:
-    adder(cases, N, test + 1, output, maxdepth, 1)
+    adder(cases, N, test + 1, output, maxframes, 1)
 
-    # Start another recursion of `maxdepth` number of `adder` function calls, if there are still test cases left.
-    if test + maxdepth < N:
-        # Call `adder` function if number of untreated test cases no longer exceeds `maxdepth + 1`:
-        if N - (test + maxdepth) <= maxdepth + 1:
-            # Do `test + maxdepth` for current test case, and reset `depth`:
-            adder(cases, N, test + maxdepth + 1, output, maxdepth + 1, 1)
-        # Apply `recursion_manager` function if number of untreated test cases still exceeds `maxdepth + 1`:
+    # Start another recursion of `maxframes` number of `adder` function calls, if there are still test cases left.
+    if test + maxframes < N:
+        # Call `adder` function if number of untreated test cases no longer exceeds `maxframes + 1`:
+        if N - (test + maxframes) <= maxframes + 1:
+            # Do `test + maxframes` for current test case, and reset `frames` for new call stack:
+            adder(cases, N, test + maxframes + 1, output, maxframes + 1, 1)
+        # Apply `recursion_manager` function if number of untreated test cases still exceeds `maxframes + 1`:
         else:
-            recursion_manager(cases, N, test + maxdepth, output, maxdepth)
+            recursion_manager(cases, N, test + maxframes, output, maxframes)
 
 def main():
     """
@@ -93,23 +94,23 @@ def main():
 
     if N: # If there are test cases
         # To optimize space complexity, implement a recursion manager to manage `adder`'s call stack,
-        # ensuring it never exceeds a set maximum recursion depth (`maxdepth`).
+        # ensuring it never exceeds a set maximum number of stack frames (`maxframes`).
 
-        # Set maximum recursion depth between 3 and 27.
+        # Set maximum number of stack frames between 3 and 27.
         # This range of values seemed optimal, recording a 0.000MB memory usage,
         # after memory profiling during testing with 100 test cases:
-        maxdepth = 10
+        maxframes = 10
 
-        if N <= maxdepth + 1: # Call `adder` function directly if `N` does not exceed `maxdepth + 1`.
-            adder(cases, N, test + 1, output, maxdepth + 1, 1)
-        else: # Apply `recursion_manager` function if `N` exceeds `maxdepth + 1`.
-            recursion_manager(cases, N, test, output, maxdepth)
+        if N <= maxframes + 1: # Call `adder` function directly if `N` does not exceed `maxframes + 1`.
+            adder(cases, N, test + 1, output, maxframes + 1, 1)
+        else: # Apply `recursion_manager` function if `N` exceeds `maxframes + 1`.
+            recursion_manager(cases, N, test, output, maxframes)
 
         # Note 1:
         # To ensure this alogrithm's recursion depth is not unnecessarily increased
-        # by 1 underlying `recursion_manager` function call, for `N <= maxdepth + 1`,
-        # `adder` is rather called directly for such cases where `N <= maxdepth + 1`.
-        # `maxdepth + 1`, because optimality does not actually start until `maxdepth + 2`
+        # by 1 underlying `recursion_manager` function call, for `N <= maxframes + 1`,
+        # `adder` is rather called directly for such cases where `N <= maxframes + 1`.
+        # `maxframes + 1`, because optimality does not actually start until `maxframes + 2`
 
         # Note 2: `output` object is passed by reference for a more efficient update to `output` across recursions.
 
